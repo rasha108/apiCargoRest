38c4e93c8865f7788a560aeafa4312eee024639d
ribikbul <ribikbul@mts.ru>
2019-10-16 19:59:34 +0300

    fix

---
+++ migrations/create_user.sql
@@ -0,0 +1,24 @@
+-- +migrate Up
+-- +migrate StatementBegin
+
+-- object: public.create_users | type: TABLE --
+-- DROP TABLE IF EXISTS public.create_users CASCADE;
+CREATE TABLE public.create_users (
+	id bigserial NOT NULL,
+	first_name varchar(64) NOT NULL,
+	last_name varchar(128) NOT NULL,
+	email varchar(128) NOT NULL,
+	encrypted_password varchar NOT NULL,
+	CONSTRAINT first_name_last_name_uq_idx UNIQUE (first_name, last_name),
+	CONSTRAINT create_users_pk PRIMARY KEY (id)
+);
+-- ddl-end --
+-- +migrate StatementEnd
+
+-- +migrate Down
+-- +migrate StatementBegin
+ALTER TABLE public.create_users OWNER TO postgres;
+-- ddl-end --
+-- +migrate StatementEnd
+
+
